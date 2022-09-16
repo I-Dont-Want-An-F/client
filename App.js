0b28f083df6d516841a160225927be5a68d8f088
Eli Lewis <jan@Elis-MacBook-Air.local>
2022-09-16 15:30:40 -0400

    changed title and message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Please don't give us an F!</Text>
       <StatusBar style="auto" />
     </View>
   );
