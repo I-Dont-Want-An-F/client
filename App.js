f1ce6d175c98cf3954ed22831f47c40a78740d11
Eli Lewis <jan@Elis-MacBook-Air.local>
2022-09-30 15:20:01 -0400

    this is an example pr to show coding togethor

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Please don't give us an F!</Text>
+      <Text>This is a code change!</Text>
       <StatusBar style="auto" />
     </View>
   );
