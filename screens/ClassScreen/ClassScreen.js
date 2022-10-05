633139487ab338db309ff9dcedac7d8b0467da46
Eli <etl3@calvin.edu>
2022-10-05 16:29:10 -0400

    super rough draft of a classcontainerelement

--- screens/ClassScreen/ClassScreen.js
+++ screens/ClassScreen/ClassScreen.js
@@ -2,12 +2,24 @@ import {React,  useState } from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
 import { GlobalStyles } from '../../stlyes/Global';
 
+//super rough draft
+
+const classList = [{name: "cs 262", key: 0}, {name: "engr 220", key: 1},];
+const listItems = classList.map((c) =>
+  < ClassContainerElemenet key={c.key} c={c.name} />
+);
+
+function ClassContainerElemenet (props) {
+  return (
+    <Text> {props.c}</Text>
+  )
+}
 
 export default function ClassScreen() {
     return (
-        <View style={{padding: 10}}>
-          <Text style={GlobalStyles.text} > This is the class screen </Text>
-        </View> 
+      <View>
+        {listItems}
+      </View>
     )
 }
 
