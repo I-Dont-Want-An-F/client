8bb634e6d6c0aa67a2bc67c51d274e1cd58c8224
Eli <etl3@calvin.edu>
2022-10-05 16:12:09 -0400

    Put search bar into header

--- screens/ClassScreen/ClassScreen.js
+++ screens/ClassScreen/ClassScreen.js
@@ -3,25 +3,11 @@ import { Text, View, TextInput, Button } from 'react-native';
 import { GlobalStyles } from '../../stlyes/Global';
 
 
-export default function ClassScreen({ navigation }) {
-  const [text, setText] = useState();
-  setText("");
+export default function ClassScreen() {
     return (
         <View style={{padding: 10}}>
-        <TextInput
-          style={GlobalStyles.text2}
-          placeholder="Type here to search for a class!"
-          onChangeText={ newText=> setText(newText) }   // this works with test 2
-          defaultValue={text}
-        />
-          <Button
-            color="gray"
-            title="search"
-            onPress= {()=> navigation.navigate(text)}
-            />
-        <Text style={GlobalStyles.text} > This is the class screen </Text>
-
-        </View>
-         
+          <Text style={GlobalStyles.text} > This is the class screen </Text>
+        </View> 
     )
 }
+
