7a3703badfe66bc8f179cae2b083ce8500194406
sza <sza@ZeAideMacBook-Air.local>
2022-10-23 22:28:14 -0400

    add class info on home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,40 @@
-import { React } from 'react';
-import { View } from 'react-native';
+import { React, useState } from 'react';
+import { Button, View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
 import Post from '../shared/Post';
-
-
 
 export default function HomeScreen ({ navigation }){
 
+    const classList = [
+        {number: "CS 262", name: "Software Engineering", key: 0,
+        prof: "Keith VanderLinden",
+        rating: "4.8", hw: "weekly", dif: "3.2", book: "no",},
+        {number:"CS 112", name: "Intro to Data Structures", key: 1,
+        prof: "Victor Norman",
+        rating: "4.5", hw: "weekly", dif:"2.9", book: "no",},
+        {number: "ENGR 220", name: "Intro to Computer Architecture", key: 2,
+        prof: "Mark Michmerhuizen",
+        rating: "2.1", hw: "never", dif:"4.3", book:"idk",}
+    ];
+
+    const listItems = classList.map((c) =>
+    < ClassContainerElemenet key={c.key} c={c.number} />);
+
+    function ClassContainerElemenet (props) {
+        const [text, setText] = useState();
+        setText(props.c)
+        return (
+          <Text> {text} </Text>
+        )
+      }
+
     return (
         <View>
-            <Post title="Example Post" text="This is an example post being rendered in a post container"/>
-            <Post title="Example Post" text="This is an example post being rendered in a post container"/>
-            <Post title="Example Post" text="This is an example post being rendered in a post container"/>
-
+          <FlatList data={classList} renderItem={({ item })=> (  
+            //  <TouchableOpacity onPress= {()=> navigation.navigate("Classes", item)}> 
+                <Post title = {item.number}
+                text = {item.name + '\n' + "Instructor: " + item.prof} ></Post>
+            //  </TouchableOpacity>                                           
+         )} />   
         </View>
-    );
+    )
 }
