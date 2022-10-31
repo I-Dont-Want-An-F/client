9a1fa5d157bc4a617b8061ad855a2b36858dd2c0
dylanmount <dcm22@calvin.edu>
2022-10-30 21:25:34 -0400

    added some basic features

---
+++ shared/GloabalStyles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const GlobalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#333',
+    },
+});
