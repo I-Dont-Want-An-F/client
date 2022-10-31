e365f6303bafbca6f3bbd02a1511a01f6562720e
Eli <etl3@calvin.edu>
2022-10-31 12:17:35 -0400

    search bar added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,31 +4,33 @@ import Post from '../shared/Post';
 
 export default function HomeScreen ({ navigation }){
 
-    const classList = [
-        {number: "CS 262", name: "Software Engineering", key: 0,
-        prof: "Keith VanderLinden",
-        rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
-        post1: 'user123: I love learning JavaScript!'},
-        {number:"CS 112", name: "Intro to Data Structures", key: 1,
-        prof: "Victor Norman",
-        rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
-        post1: 'user456: Any tips for studying for next test?'},
-        {number: "ENGR 220", name: "Intro to Computer Architecture", key: 2,
-        prof: "Mark Michmerhuizen",
-        rating: "2.1", hw: "never", dif:"4.3", book:"No info",
-        post1: 'user789: When is the next lab help session?'}
-    ];
-
-    const listItems = classList.map((c) =>
-    < ClassContainerElemenet key={c.key} c={c.number} />);
-
-    function ClassContainerElemenet (props) {
-        const [text, setText] = useState();
-        setText(props.c)
-        return (
-          <Text> {text} </Text>
-        )
-      }
+  const classList = [
+    {number: "CS 262", name: "Software Engineering", key: 0,
+    prof: "Keith VanderLinden",
+    rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
+    post1: 'user123: I love learning JavaScript!'},
+    {number:"CS 112", name: "Intro to Data Structures", key: 1,
+    prof: "Victor Norman",
+    rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
+    post1: 'user456: Any tips for studying for next test?'},
+    {number: "ENGR 220", name: "Intro to Computer Architecture", key: 2,
+    prof: "Mark Michmerhuizen",
+    rating: "2.1", hw: "never", dif:"4.3", book:"No info",
+    post1: 'user789: When is the next lab help session?'},
+    {number: "CS 102", name: "Software Engineering", key: 3,
+    prof: "Keith VanderLinden",
+    rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
+    post1: 'user123: I love learning JavaScript!'},
+    {number:"ENGR 101", name: "Intro to Data Structures", key: 4,
+    prof: "Victor Norman",
+    rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
+    post1: 'user456: Any tips for studying for next test?'},
+    {number: "Math 271", name: "Intro to Computer Architecture", key: 5,
+    prof: "Mark Michmerhuizen",
+    rating: "2.1", hw: "never", dif:"4.3", book:"No info",
+    post1: 'user789: When is the next lab help session?'}
+    
+];
 
     return (
         <View>
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -8,35 +8,35 @@ import {GlobalStyles} from '../shared/GlobalStyles';
 export default function ProfileScreen ({ navigation }){
     //classes user is taken 
     const classList = [
-        {number: "CS 262", name: "Software Engineering", key: 0,
-        prof: "Keith VanderLinden",
-        rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
-        post1: 'user123: I love learning JavaScript!'},
-        {number:"CS 112", name: "Intro to Data Structures", key: 1,
-        prof: "Victor Norman",
-        rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
-        post1: 'user456: Any tips for studying for next test?'},
-        {number: "ENGR 220", name: "Intro to Computer Architecture", key: 2,
-        prof: "Mark Michmerhuizen",
-        rating: "2.1", hw: "never", dif:"4.3", book:"No info",
-        post1: 'user789: When is the next lab help session?'}
-        
-    ];
-    const classList2 = [
-        {number: "CS 102", name: "Software Engineering", key: 0,
-        prof: "Keith VanderLinden",
-        rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
-        post1: 'user123: I love learning JavaScript!'},
-        {number:"ENGR 101", name: "Intro to Data Structures", key: 1,
-        prof: "Victor Norman",
-        rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
-        post1: 'user456: Any tips for studying for next test?'},
-        {number: "Math 271", name: "Intro to Computer Architecture", key: 2,
-        prof: "Mark Michmerhuizen",
-        rating: "2.1", hw: "never", dif:"4.3", book:"No info",
-        post1: 'user789: When is the next lab help session?'}
-        
-    ];
+      {number: "CS 262", name: "Software Engineering", key: 0,
+      prof: "Keith VanderLinden",
+      rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
+      post1: 'user123: I love learning JavaScript!'},
+      {number:"CS 112", name: "Intro to Data Structures", key: 1,
+      prof: "Victor Norman",
+      rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
+      post1: 'user456: Any tips for studying for next test?'},
+      {number: "ENGR 220", name: "Intro to Computer Architecture", key: 2,
+      prof: "Mark Michmerhuizen",
+      rating: "2.1", hw: "never", dif:"4.3", book:"No info",
+      post1: 'user789: When is the next lab help session?'}
+      
+  ];
+  const classList2 = [
+      {number: "CS 102", name: "Software Engineering", key: 0,
+      prof: "Keith VanderLinden",
+      rating: "4.8", hw: "weekly", dif: "3.2", book: "Not required",
+      post1: 'user123: I love learning JavaScript!'},
+      {number:"ENGR 101", name: "Intro to Data Structures", key: 1,
+      prof: "Victor Norman",
+      rating: "4.5", hw: "weekly", dif:"2.9", book: "Not required, but recommended",
+      post1: 'user456: Any tips for studying for next test?'},
+      {number: "Math 271", name: "Intro to Computer Architecture", key: 2,
+      prof: "Mark Michmerhuizen",
+      rating: "2.1", hw: "never", dif:"4.3", book:"No info",
+      post1: 'user789: When is the next lab help session?'}
+      
+  ];
 
     const listItems = classList.map((c) =>
     < ClassContainerElemenet key={c.key} c={c.number} />);
