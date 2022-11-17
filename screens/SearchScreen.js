3eb6b85f66c55efe55d13f677acbf47766932971
dylanmount <dcm22@calvin.edu>
2022-11-16 23:18:29 -0500

    kinda working

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,90 +1,88 @@
-//this is the page that shows the search bar & created by Dylan
-//based off a tutorial by Kevin Thomas 
-//https://blog.logrocket.com/create-react-native-search-bar-from-scratch/
+// this is the page that shows the search bar & created by Dylan
+// based off a tutorial by Kevin Thomas 
+// https://blog.logrocket.com/create-react-native-search-bar-from-scratch/
 
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, FlatList, View, TouchableOpacity, SafeAreaView, TextInput, Keyboard, Button, ActivityIndicator } from "react-native";
+// TO-Do 
+//Fix the keyboard as it keeps dropping down 
+//Fix the search as it does not like numbers 
+//Change the style to make it look beter 
+
+import { React, useState, useEffect } from "react";
+import { StyleSheet, Text, FlatList, View, TouchableOpacity, SafeAreaView, TextInput,Keyboard} from "react-native";
 //import List from "./SearchData";
 import { Feather, Entypo } from "@expo/vector-icons";
 import { GlobalStyles } from '../shared/GlobalStyles';
 //import SearchBar from "./SearchBar";
 
-//creates an item??
-const Item = ({ name, details }) => (
-  <View //style={styles.item}>
-  >
-    <Text style={GlobalStyles.titleSmall}>{name}</Text>
-    <Text style={GlobalStyles.titleSmall}>{details}</Text>
-  </View>
-);
+export default function SearchScreen({ navigation }) {
 
-//filters the list of classes and creates a flatlist 
-const List = ({ searchPhrase, data }) => {
-  const renderItem = ({ item }) => {
-    // when no input, show all
-    if (searchPhrase === "") {
-      return (<View style={GlobalStyles.background2}>
-        <TouchableOpacity onPress={() => navigation.navigate("Post", item)}>
-          <Text style={GlobalStyles.textSmall}> {item.shortname}</Text>
-        </TouchableOpacity>
-      </View>
-      )
+  //filters the list of classes and creates a flatlist 
+  const List = ({ searchPhrase, data }) => {
+    const renderItem = ({ item }) => {
+      // when no input, show all
+      if (searchPhrase === "") {
+        return (<View style={GlobalStyles.background2}>
+          <TouchableOpacity onPress={() => navigation.navigate("Details", item)}>
+            <Text style={GlobalStyles.textSmall}> {item.shortname}: {item.longname}</Text>
+          </TouchableOpacity>
+        </View>
+        )
+      }
+      // filter by the shortname
+      if (item.shortname.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+        return (<View style={GlobalStyles.background2}>
+          <TouchableOpacity onPress={() => navigation.navigate("Details", item)}>
+            <Text style={GlobalStyles.textSmall}> {item.shortname}: {item.longname}</Text>
+          </TouchableOpacity>
+        </View>
+        )
+      }
+      // filter by the subject 
+      if (item.subject.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
+        return (<View style={GlobalStyles.background2}>
+          <TouchableOpacity onPress={() => navigation.navigate("Details", item)}>
+            <Text style={GlobalStyles.textSmall}>{item.shortname}: {item.longname}</Text>
+          </TouchableOpacity>
+        </View>
+        )
+      }
     }
-    // filter by the shortname
-    if (item.shortname.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
-      return (<View style={GlobalStyles.background2}>
-        <TouchableOpacity onPress={() => navigation.navigate("Post", item)}>
-          <Text style={GlobalStyles.textSmall}> {item.shortname}</Text>
-        </TouchableOpacity>
-      </View>
-      )
-    }
-    // filter by the subject 
-    if (item.subject.toUpperCase().includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))) {
-      return (<View style={GlobalStyles.background2}>
-        <TouchableOpacity onPress={() => navigation.navigate("Post", item)}>
-          <Text style={GlobalStyles.textSmall}> {item.shortname}</Text>
-        </TouchableOpacity>
-      </View>
-      )
-    }
-  }
-  return (
-    <SafeAreaView style={GlobalStyles.container2}>
-      <View>
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
+    return (
+      <SafeAreaView style={GlobalStyles.container2}>
+        <View>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  };
+
+  //creates a search bar 
+  const SearchBar = ({ searchPhrase, setSearchPhrase }) => {
+    return (
+      <View style={GlobalStyles.container2}>
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        {/* Input field */}
+        <TextInput
+          style={GlobalStyles.textSmall}
+          placeholder="Search"
+          value={searchPhrase}
+          onChangeText={setSearchPhrase}
         />
       </View>
-    </SafeAreaView>
-  );
-};
-//creates a search bar 
-const SearchBar = ({ searchPhrase, setSearchPhrase }) => {
-  return (
-    <View style={GlobalStyles.container2}>
-      {/* search Icon */}
-      <Feather
-        name="search"
-        size={20}
-        color="black"
-        style={{ marginLeft: 1 }}
-      />
-      {/* Input field */}
-      <TextInput
-        style={GlobalStyles.textSmall}
-        placeholder="Search"
-        value={searchPhrase}
-        onChangeText={setSearchPhrase}
-      />
-    </View>
-  );
-};
+    );
+  };
 
-//calls the other functions and creates the SearchScreen
-const Home = () => {
+//creates the searchscreen and calls the funtcions above
   const [searchPhrase, setSearchPhrase] = useState("");
   const [fakeData, setFakeData] = useState();
 
@@ -112,9 +110,8 @@ const Home = () => {
         searchPhrase={searchPhrase}
         data={fakeData}
       />
-
     </SafeAreaView>
   );
 };
 
-export default Home;
+
